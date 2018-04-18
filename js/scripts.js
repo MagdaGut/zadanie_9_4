70e7667aa5cf0608bd4cf6c384e240e69d2a0fdf
Magda Gutowska <m.gutowska0909@gmail.com>
2018-04-18 21:16:54 +0200

    Change script.js

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+// scripts.js
+
+var i, j;
+var row = prompt('Podaj wysokość choinki w wierszach');
+
+function drawTree (row){
+	for (i=1; i<row; i++)
+{
+    for (j=1; j<row; j++)
+    {
+    console.log("*");
+    }
+}
+console.log("<br>");
+	for (i=row-1, i>=0, i=--){
+
+	}
+}
